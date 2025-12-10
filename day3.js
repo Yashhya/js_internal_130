ffe28a495807c0d9f3a6af94e482d4b47e39a668
Yash Yadav <yash9648444689@gmail.com>
2025-12-10 05:19:23 +0000

    day3

---
+++ day3.js
@@ -0,0 +1,17 @@
+// let arr=[1,2,3,"apple","mango","ch"]
+// console.log(arr)
+
+// let arr=["mango","orange","pine","apple","banana"]
+// console.log(arr)
+// console.log(arr.push("papaya"))
+// console.log(arr.unshift("kiwi"))
+// console.log(arr.includes("mango"))
+// console.log(arr.indexOf("banana"))
+
+
+// let arr=[2,4,6,8]
+// console.log(arr.map(n=>n*2))
+// let r1=[10,25,30,5,60]
+// console.log(r1.filter(n=>n>20))
+// const sum=arr.reduce((acc,val)=>acc+val,0)
+// console.log(sum)
